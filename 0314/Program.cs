6ec1ac7855c3e820aac33f81af2af21cc37fee50
user <user@DESKTOP-6JBEF5K>
2022-03-14 11:24:01 +0800

    加入專案檔案。

---
+++ 0314/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _0314
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  int sum = 0;
+            //for (int i = 1; i <= 10; i++)
+            //  {
+            //      sum += i;
+            //  }
+            //  Console.WriteLine(sum);
+
+            //練習1
+            //for (int i = 0; i <= 5; i++)
+            //{
+            //    for (int j = 0; j <= 5; j++)
+            //    {
+            //        if (i > j)
+            //        {
+            //            Console.Write("*");
+            //        }
+            //        else
+            //        {
+            //            break;
+            //        }
+            //    }
+            //    Console.WriteLine();
+            //}
+            //Console.Read();
+
+            //練習2
+            //string input = Console.ReadLine();
+            //int inputInt = int.Parse(input);
+            //for (int i=0; i <= inputInt; i++)
+            //{
+            //    Console.WriteLine("N*"+i);
+            //}
+
+            //練習3
+            //for(int i = 1; i <= 9; i++)
+            //{
+            //    for(int j=1; j<=9; j++)
+            //    {
+            //        Console.WriteLine(i + "*" + j + "=" +i*j);
+            //    }
+            //}
+        }
+       
+
+    }
+}
